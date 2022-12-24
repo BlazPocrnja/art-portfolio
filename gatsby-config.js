429fcfe3bcd1db41bc894acde761ae74ab276008
Blaz Pocrnja <bpocrnja@gmail.com>
2022-12-24 12:44:16 -0700

    [Logo] Added my own logo 🙂

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteTitle: `Blaz Pocrnja`,
+    siteTitleAlt: `Blaz Pocrnja - Art Portfolio`,
+    siteHeadline: `Aspiring Animator and Artist`,
+    siteUrl: `https://blazpocrnja.com`,
+    siteDescription: `Art portfolio for Blaz Pocrnja.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Blaz Pocrnja`,
   },
   trailingSlash: `never`,
   plugins: [
